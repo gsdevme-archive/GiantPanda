2c7bc5d570489a7352524b4d45b5677c0c53f725
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-20 02:36:34 +0000

    Exception class for Panda & ClassNotFound

---
+++ System/Exceptions/ClassNotFoundException.php
@@ -0,0 +1,5 @@
+<?php
+
+	namespace System\Exceptions;
+
+	class ClassNotFoundException extends Exception{}
---
+++ System/Exceptions/Exception.php
@@ -0,0 +1,22 @@
+<?php
+
+	namespace System\Exceptions;
+
+	abstract class Exception extends \Exception
+	{
+		
+		private $_pandaCode;
+
+		public function __construct($message, $code=null, \Exception $e=null, $pandaCode=500)
+		{
+			$this->_pandaCode = $pandaCode;
+
+			parent::__construct($message, $code, $e);
+			
+		}
+
+		public function getPandaCode()
+		{
+			return $this->_pandaCode;
+		}
+	}
