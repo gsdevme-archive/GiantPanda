4b79116a1fafec7859f5a17c6438dfe777739919
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-22 14:48:42 +0000

    Added debugCallback

--- Index/AppConfig.php
+++ Index/AppConfig.php
@@ -2,6 +2,10 @@
 
 	return array(
 		'debug' => (bool)true,
+		'debugCallback' => (function($errors, $panda){
+			echo 'Your Error Here :), perhaps call a Error Controller ?';	
+			exit;		
+		}),
 
 		'defaultController' => 'Home',
 		'defaultMethod' => 'index',
