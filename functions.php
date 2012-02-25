c808b7d751a828151864864aca0b97982956b2a7
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-25 04:52:10 +0000

    Added ifsetor function

--- functions.php
+++ functions.php
@@ -5,3 +5,16 @@
 	 *
 	 * @author Gavin Staniforth <Email:gsdev@me.com> <Arpanet:http://gsdev.me> @gsphpdev
 	 */	
+
+	/**
+     * This function adds the purposed language construct detailed here
+     * https://wiki.php.net/rfc/ifsetor
+     * 
+     * @param mixed $value
+     * @param mixed $or
+     * @return mixed 
+     */
+	function ifsetor(&$value, $or=null)
+	{
+		return (isset($value)) ? $value : $or;
+	}	
