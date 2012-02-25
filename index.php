0aca15d4e050665564a21656a15e17cac22ee61c
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-25 18:48:14 +0000

    added some comments and format

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	define('PANDA_MEMORY', memory_get_usage());
 	define('PANDA_TIME', microtime(true));
 
-	/**
+/**
 	 * Index.php the target for our web server.
 	 *
 	 * In terms of license to use the Framework, use it as you want. 
@@ -21,7 +21,7 @@
 	use \Panda\System\ControllerFactory;
 	use \Panda\System\ExceptionHandler;
 
-	$root = realpath(dirname(__FILE__)) . '/';
+$root = realpath(dirname(__FILE__)) . '/';
 
 	require_once $root . 'bootstrap.php';
 	require_once $root . 'functions.php';
@@ -29,29 +29,29 @@
 	require_once $root . 'Panda/System/Panda.php';
 
 	$panda = Panda::getInstance(array(
-		'root' => $root,
-		'application' => 'Index',
-		'request' => $_SERVER['REQUEST_URI'],
-		'host' => $_SERVER['HTTP_HOST'],
-		'file' => $_SERVER['SCRIPT_NAME'],
-		'memory' => PANDA_MEMORY,
-		'time' => PANDA_TIME,
-		// The properties below are to be overrided within your AppConfig
-		'debug' => ( bool ) false,
-		'defaultController' => 'Home',
-		'defaultMethod' => 'index',
-		'appRegistry' => ( bool ) true,
-	));
+			'root' => $root,
+			'application' => 'Index',
+			'request' => $_SERVER['REQUEST_URI'],
+			'host' => $_SERVER['HTTP_HOST'],
+			'file' => $_SERVER['SCRIPT_NAME'],
+			'memory' => PANDA_MEMORY,
+			'time' => PANDA_TIME,
+			// The properties below are to be overrided within your AppConfig
+			'debug' => ( bool ) false,
+			'defaultController' => 'Home',
+			'defaultMethod' => 'index',
+			'appRegistry' => ( bool ) true,
+		));
 
-	try{
+	try {
 		$request = new Request($panda);
 		$request->handleRequest();
 
 		$router = new Router($request, $panda);
-		ControllerFactory::create($router->getRoute(), $router->isDirectory());	
+		ControllerFactory::create($router->getRoute(), $router->isDirectory());
 
-		unset($request, $router);	
-	}catch(Exception $e){
+		unset($request, $router);
+	} catch (Exception $e) {
 		$exception = new ExceptionHandler($e, $panda);
 		$exception->handle();
 		$exception->shutdown();
