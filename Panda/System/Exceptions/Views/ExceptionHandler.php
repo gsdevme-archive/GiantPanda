fbc260a6efecda2d76c611e90a7a529d29aadfb9
Gavin Staniforth <gavshouse90@gmail.com>
2012-03-05 06:43:59 +0000

    Added a html title with "Error"

--- Panda/System/Exceptions/Views/ExceptionHandler.php
+++ Panda/System/Exceptions/Views/ExceptionHandler.php
@@ -5,6 +5,8 @@ __error__
 		<style>
 			body,html{background:#FAFAFA;font-family:Helvetica, Arial, sans-serif;font-weight:400;font-size:13px;margin:0;padding:0}.error{background:#DEDEDE}.error h1{font-size:14px;font-weight:400;background:#606B7C;line-height:32px;color:#FAFAFA;text-indent:5px;margin:0;padding:0 10px}.error:first-child h1{background:#8B3E2F;font-size:18px;line-height:42px}.error .code{width:100%;padding:10px 1%}.error .code h3{line-height:11px;font-size:11px;color:#303030;margin:0;padding:0}.error .code pre{display:block;width:96%;font-size:12px;font-family:monospace;white-space:pre-wrap;background:#FFF;padding:5px 1%}
 		</style>
+
+		<title>Error</title>
 	</head>
 
 	<body>
