2553d5673021f01dec423d24f4c1a1cefc657e3f
Gavin Staniforth <gavshouse90@gmail.com>
2012-03-06 15:50:16 +0000

    Removed bootstrap file, added contents into index.php

--- index.php
+++ index.php
@@ -18,16 +18,33 @@
 	use \Panda\System\ControllerFactory;
 	use \Panda\System\ExceptionHandler;
 
+	if (PHP_VERSION_ID < 50303)
+		throw new Exception('Your trying to run me on a PHP version below PHP 5.3.3... how dare you !');
+
+	// Real programming erors
+	set_error_handler(function($errno, $errstr, $errfile, $errline ) {
+			throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
+		});
+
+	// Disable magic quotes (Lovely magic quotes enabled on the server.... a developers worst nightmare)
+	if (get_magic_quotes_gpc())
+		ini_set('magic_quotes_gpc', 0);
+
+	// This is mainly when running under CLI, its better to just NULL them then get undefined index all over the place..
+	if (!isset($_SERVER['HTTP_HOST'], $_SERVER['REQUEST_URI'])) {
+		$_SERVER['REQUEST_URI'] = null;
+		$_SERVER['HTTP_HOST'] = null;
+	}	
+
 	$root = realpath(dirname(__FILE__)) . '/';
 
-	require $root . 'bootstrap.php';
 	require $root . 'functions.php';
 	require $root . 'Panda/System/Registry.php';
 	require $root . 'Panda/System/Panda.php';
 
 	$panda = Panda::getInstance(array(
 			'root' => $root,
-			'application' => 'Install',
+			'application' => 'Index',
 			'request' => $_SERVER['REQUEST_URI'],
 			'host' => $_SERVER['HTTP_HOST'],
 			'file' => $_SERVER['SCRIPT_NAME'],
